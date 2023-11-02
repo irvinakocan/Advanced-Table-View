bcaeab658765649803013b9f717d30df077a5e7b
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-11-02 18:03:02 +0100

    Initial Commit

---
+++ Advanced Table View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Advanced Table View
+//
+//  Created by Macbook Air 2017 on 2. 11. 2023..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
